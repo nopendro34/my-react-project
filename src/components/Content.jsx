0c326de12779b0a8c88c76698aceeda1878ab04d
Nopendro Pakpahan <nopendro45@outlook.com>
2023-09-27 11:46:31 +0700

    Membuat element content

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import '../style.css'
+
+function Content() {
+	return(
+		<div className="content">
+		<h2>Content</h2>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni unde blanditiis id, necessitatibus omnis tempora libero illo voluptatem ab corporis, maiores voluptas. Sit autem vero iste quis dolore! Voluptatibus quam sapiente dicta id, incidunt veniam corporis possimus eos quaerat distinctio facilis recusandae maiores.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolore quasi vitae!</p>
+		</div>
+	)
+}
+
+export default Content
