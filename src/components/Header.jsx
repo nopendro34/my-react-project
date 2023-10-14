ae322824ed9d465c263f39a2437fdf1834a436df
Nopendro Pakpahan <nopendro45@outlook.com>
2023-10-14 19:07:43 +0700

    Mengintegrasikan Prettier Dan ESlint Dan Melakukan Formating Code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import '../style.css'
+import "../style.css";
 
 function Header() {
-	return(
-		<div className="header">
-		<h1>Halo Semuanya</h1>
-		<p>Ini Project React Pertama</p>
-		</div>
-	)
+  return (
+    <div className="header">
+      <h1>Halo Semuanya</h1>
+      <p>Ini Project React Pertama</p>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Header from './components/Header'
-import Content from './components/Content'
-import './style.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <Content />
   </React.StrictMode>,
-)
+);
